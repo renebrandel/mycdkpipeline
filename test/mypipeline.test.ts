af3e0df2afc4dc8be45e61f3a8ab5e6da542660e
Rene Brandel <renbran@amazon.com>
2021-11-17 07:24:17 -0800

    Initial commit

---
+++ test/mypipeline.test.ts
@@ -0,0 +1,13 @@
+import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as Mypipeline from '../lib/mypipeline-stack';
+
+test('Empty Stack', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new Mypipeline.MypipelineStack(app, 'MyTestStack');
+    // THEN
+    expectCDK(stack).to(matchTemplate({
+      "Resources": {}
+    }, MatchStyle.EXACT));
+});
