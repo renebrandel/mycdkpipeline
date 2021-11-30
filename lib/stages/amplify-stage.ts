07178ba93c1e1c43fd4a2f809e6a77473b2c2396
Rene Brandel <renbran@amazon.com>
2021-11-30 13:57:49 -0800

    added new stack

--- lib/stages/amplify-stage.ts
+++ lib/stages/amplify-stage.ts
@@ -1,6 +1,6 @@
 import { CfnOutput, cfnTagToCloudFormation, Construct, Stage, StageProps } from '@aws-cdk/core';
-import { AmplifyExportedBackend } from '@aws-amplify/cdk-exported-backend';
 import { CfnBucket } from '@aws-cdk/aws-s3';
+import { AmplifyExportedBackend } from '@aws-amplify/cdk-exported-backend';
 import * as path from 'path'
 import * as cdk from '@aws-cdk/core'
 
@@ -9,10 +9,9 @@ export class AmplifyStage extends Stage {
   constructor(scope: Construct, id: string, props?: StageProps) {
     super(scope, id, props);
 
-    
     // ADD AMPLIFY EXPORTED BACKEND STACK HERE
-    const amplifyStack = new AmplifyExportedBackend(this, "amplifyexportedbackend", {
-      path: path.resolve(__dirname, '..', 'amplify-export-mytodoapp'),
+    const amplifyStack = new AmplifyExportedBackend(this, "amplify-stack", {
+      path: path.resolve(__dirname, "..", "stacks", "amplify-export-demosession"),
       amplifyEnvironment: "dev"
     })
   }
